e353e0dd6279c7b17b294ade6137d4b20c89b51d
GabrielSampaio1602 <gabriel_1602@hotmail.com>
2023-08-01 18:00:36 -0300

    build: Changing sync functions to async

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import os
 from kivy.lang import Builder
 import requests
 from sensitive_values import BDLINK, DDNS, SV_PORT
+from kivy.app import App
+import asks
 
 
 def load_kv_path(path):
@@ -14,15 +16,27 @@ def load_kv_path(path):
 
 
 def contact_server(command, func_popup_server_off):
+    app = App.get_running_app()
+    app.nursery.start_soon(async_contact_server, command, func_popup_server_off)
+
+
+async def async_contact_server(command, func_popup_server_off):
     # Send initiate command to server
+    session = asks.Session()
     try:
         try:
-            host = requests.get(f"{BDLINK}/Running_Info/.json").json()["Server_DNS"]
-            req = requests.get(f"http://{host}:{SV_PORT}/{command}", timeout=5)
+            # host = requests.get(f"{BDLINK}/Running_Info/.json").json()["Server_DNS"]
+            # req = requests.get(f"http://{host}:{SV_PORT}/{command}", timeout=5)
+            requisicao_host = await session.get(f"{BDLINK}/Running_Info/.json")
+            host = requisicao_host.json()["Server_DNS"]
+            req = await session.get(f"http://{host}:{SV_PORT}/{command}", timeout=5)
         except requests.exceptions.InvalidSchema:
             try:
-                host = requests.get(f"{BDLINK}/Running_Info/.json").json()["Server_IP"]
-                req = requests.get(f"http://{host}:{SV_PORT}/{command}", timeout=5)
+                # host = requests.get(f"{BDLINK}/Running_Info/.json").json()["Server_IP"]
+                # req = requests.get(f"http://{host}:{SV_PORT}/{command}", timeout=5)
+                requisicao_host = await session.get(f"{BDLINK}/Running_Info/.json")
+                host = requisicao_host.json()["Server_IP"]
+                req = await session.get(f"http://{host}:{SV_PORT}/{command}", timeout=5)
             except requests.exceptions.InvalidSchema:
                 return func_popup_server_off
         return req
